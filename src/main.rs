70390aa6852612817575a2c521a1b405a3b2b3bf
Manoel Brunnen <mb@lee-brunnen.de>
2021-12-05 11:39:04 +0100

    Parse day to u32

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-mod utils;
 mod days;
+mod utils;
 
 use clap::{App, Arg};
-use std::process;
-use utils::input::Input;
-use utils::challenge::Challenge;
 use days::*;
+use std::process;
+use utils::challenge::Challenge;
+use utils::input::Input;
 
 fn main() {
     let matches = App::new("Advent of Code 2021")
@@ -13,7 +13,8 @@ fn main() {
             Arg::with_name("DAY")
                 .help("Sets the day")
                 .required(true)
-                .index(1),
+                .index(1)
+                .multiple(false),
         )
         .arg(
             Arg::with_name("INPUT")
@@ -23,27 +24,35 @@ fn main() {
         )
         .get_matches();
 
-    let day = matches.value_of("DAY");
+    let day: u32 = matches
+        .value_of("DAY")
+        .unwrap_or_else(|| {
+            eprintln!("Problem parsing DAY: {:#?}", matches);
+            process::exit(1);
+        })
+        .parse::<u32>()
+        .unwrap_or_else(|err| {
+            eprintln!("Problem converting DAY: {:#?}", err);
+            process::exit(1);
+        });
+
     let input_files: Vec<_> = matches
         .values_of("INPUT")
         .unwrap()
         .map(|s| s.to_string())
         .collect();
+
     let input = Input::load(&input_files[..]).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
     let result = match day {
-        Some("1") => {
+        1 => {
             let challenge = day1::Day1::new(input.lines);
             challenge.run()
         }
-        Some(x) => unimplemented!("Invalid day: {}", x),
-        None => {
-            eprintln!("Problem parsing arguments: {:#?}", matches);
-            process::exit(1);
-        }
+        x => unimplemented!("Invalid day: {}", x),
     }
     .unwrap_or_else(|err| {
         eprintln!("Application error: {}", err);

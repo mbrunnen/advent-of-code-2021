9a4b2442006f4cc03a284737d9847e922aeaa745
Manoel Brunnen <mb@lee-brunnen.de>
2021-12-05 23:21:05 +0100

    Add (wrong) solution for day 2

--- src/days/day2.rs
+++ src/days/day2.rs
@@ -48,6 +48,61 @@ impl Add for Point {
     }
 }
 
+#[derive(Debug, PartialEq)]
+struct SubmarineState {
+    pos: Point,
+    vel: i32,
+    aim: i32,
+}
+
+impl SubmarineState {
+    pub fn new(vel: i32, aim: i32) -> Self {
+        Self {
+            pos: Point { x: 0, y: 0 },
+            vel,
+            aim,
+        }
+    }
+}
+
+impl FromStr for SubmarineState {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let pat = Regex::new(r"^(\w+)\s(\d+)$").unwrap();
+        let token = pat.captures(s).unwrap();
+
+        let direction = token.get(1).unwrap().as_str();
+        let value = token.get(2).unwrap().as_str().parse::<i32>()?;
+
+        let (vel, aim) = match direction {
+            "forward" => (value, 0),
+            "down" => (0, value),
+            "up" => (0, -value),
+            _ => {
+                panic!("Unknown direction {}", direction)
+            }
+        };
+
+        Ok(Self::new(vel, aim))
+    }
+}
+
+impl Add for SubmarineState {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self {
+            pos: Point {
+                x: self.pos.x + other.vel,
+                y: self.pos.x + (self.aim * other.vel),
+            },
+            vel: other.vel,
+            aim: self.aim + other.aim,
+        }
+    }
+}
+
 impl Challenge for Day2 {
     fn new(input_file: &str) -> Self {
         Self {
@@ -80,6 +135,12 @@ impl Day2 {
     }
 
     fn run_part_two(&self) -> Result<String, String> {
-        Ok(format!("{:#?}", 0))
+        let end_position: SubmarineState = self
+            .data
+            .iter()
+            .map(|l| SubmarineState::from_str(l).unwrap())
+            .fold(SubmarineState::new(0, 0), |acc, item| acc + item);
+
+        Ok(format!("{:#?}", end_position.pos.x * end_position.pos.y))
     }
 }

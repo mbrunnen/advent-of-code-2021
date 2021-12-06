b51d9d142706ad4d00c89b1fabdb48eaa64fd8e4
Manoel Brunnen <mb@lee-brunnen.de>
2021-12-06 22:02:01 +0100

    Solve Day 3 part 1

--- src/days/day3.rs
+++ src/days/day3.rs
@@ -1,4 +1,5 @@
 use crate::utils::challenge::Challenge;
+use std::convert::TryInto;
 
 pub struct Day3 {
     data: Vec<String>,
@@ -26,7 +27,6 @@ impl Challenge for Day3 {
 
 impl Day3 {
     fn count_ones(nums: &[u32], bit: usize) -> usize {
-        println!("{:b}", 1 << bit);
         nums.iter()
             .map(|n| n & (1 << bit))
             .filter(|x| x > &0)
@@ -37,8 +37,37 @@ impl Day3 {
         u32::from_str_radix(s, 2).expect("Could not parse line")
     }
 
+    fn convert(d: &[String]) -> Vec<u32> {
+        d.iter().map(|s| Self::to_num(s)).collect()
+    }
+
+    fn calculate_gamma(nums: &[u32], bitwidth: usize) -> usize {
+        Self::to_num(
+            &(0..bitwidth)
+                .rev()
+                .map(|b| {
+                    if 2 * Self::count_ones(nums, b) > nums.len() {
+                        '1'
+                    } else {
+                        '0'
+                    }
+                })
+                .collect::<String>(),
+        )
+        .try_into()
+        .unwrap()
+    }
+
+    fn calculate_epsilon(gamma: usize, bitwidth: usize) -> usize {
+        gamma ^ ((1 << bitwidth) - 1)
+    }
+
     fn run_part_one(&self) -> Result<String, String> {
-        Ok(format!("{:#?}", 0))
+        let data: Vec<u32> = Self::convert(&self.data);
+        let gamma = Self::calculate_gamma(&data, 12);
+        let epsilon = Self::calculate_epsilon(gamma, 12);
+
+        Ok(format!("{:#?}", gamma * epsilon))
     }
 
     fn run_part_two(&self) -> Result<String, String> {
@@ -87,8 +116,24 @@ mod tests {
         let expected: Vec<usize> = vec![5, 7, 8, 5, 7];
 
         for (pos, exp) in expected.iter().enumerate() {
-            println!("exp[{}] = {}", pos, exp);
             assert_eq!(Day3::count_ones(&input, pos), *exp);
         }
+    }
+
+    #[test]
+    fn test_calculate_gamma() {
+        let input: Vec<u32> = vec![
+            0b00100, 0b11110, 0b10110, 0b10111, 0b10101, 0b01111, 0b00111, 0b11100, 0b10000,
+            0b11001, 0b00010, 0b01010,
+        ];
+
+        let expected = 0b10110;
+
+        assert_eq!(Day3::calculate_gamma(&input, 5), expected);
+    }
+
+    #[test]
+    fn test_calculate_epsilon() {
+        assert_eq!(Day3::calculate_epsilon(22, 5), 9);
     }
 }

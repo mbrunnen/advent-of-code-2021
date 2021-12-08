6213a40a746eb37a06168203adb90729edddbf34
Manoel Brunnen <mb@lee-brunnen.de>
2021-12-08 20:54:44 +0100

    Parse input for day 4

--- src/days/day4.rs
+++ src/days/day4.rs
@@ -4,6 +4,49 @@ pub struct Day4 {
     data: Vec<String>,
 }
 
+#[derive(Debug, PartialEq)]
+struct BingoBoard {
+    data: Vec<Vec<u32>>,
+}
+
+impl From<&[String]> for BingoBoard {
+    fn from(data: &[String]) -> Self {
+        let data: Vec<Vec<u32>> = data
+            .iter()
+            .map(|l| {
+                l.split(' ')
+                    .filter(|s| !s.is_empty())
+                    .map(|s| s.parse::<u32>().unwrap())
+                    .collect()
+            })
+            .collect();
+
+        BingoBoard { data }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+struct BingoSubsystem {
+    input: Vec<u32>,
+    boards: Vec<BingoBoard>,
+}
+
+impl From<Vec<String>> for BingoSubsystem {
+    fn from(data: Vec<String>) -> Self {
+        let input: Vec<u32> = data[0]
+            .split(',')
+            .map(|s| s.parse::<u32>().unwrap())
+            .collect();
+
+        let boards: Vec<BingoBoard> = (2..data.len())
+            .step_by(6)
+            .map(|i| BingoBoard::from(&data[i..i + 5]))
+            .collect();
+
+        BingoSubsystem { input, boards }
+    }
+}
+
 impl Challenge for Day4 {
     fn new(input_file: &str) -> Self {
         Self {
@@ -36,6 +79,7 @@ impl Day4 {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     fn get_input() -> Vec<String> {
         vec![
             String::from("7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1"),
@@ -60,7 +104,61 @@ mod tests {
         ]
     }
 
-    use super::*;
     #[test]
-    fn function_name_test() {}
+    fn test_bingo_board_from() {
+        let expected = BingoBoard {
+            data: vec![
+                vec![22, 13, 17, 11, 0],
+                vec![8, 2, 23, 4, 24],
+                vec![21, 9, 14, 16, 7],
+                vec![6, 10, 3, 18, 5],
+                vec![1, 12, 20, 15, 19],
+            ],
+        };
+
+        assert_eq!(BingoBoard::from(&get_input()[2..7]), expected);
+    }
+
+    #[test]
+    fn test_bingo_subsystem_from() {
+        let expected = BingoSubsystem {
+            input: vec![
+                7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22, 18, 20, 8,
+                19, 3, 26, 1,
+            ],
+            boards: vec![
+                BingoBoard {
+                    data: vec![
+                        vec![22, 13, 17, 11, 0],
+                        vec![8, 2, 23, 4, 24],
+                        vec![21, 9, 14, 16, 7],
+                        vec![6, 10, 3, 18, 5],
+                        vec![1, 12, 20, 15, 19],
+                    ],
+                },
+                BingoBoard {
+                    data: vec![
+                        vec![3, 15, 0, 2, 22],
+                        vec![9, 18, 13, 17, 5],
+                        vec![19, 8, 7, 25, 23],
+                        vec![20, 11, 10, 24, 4],
+                        vec![14, 21, 16, 12, 6],
+                    ],
+                },
+                BingoBoard {
+                    data: vec![
+                        vec![14, 21, 17, 24, 4],
+                        vec![10, 16, 15, 9, 19],
+                        vec![18, 8, 23, 26, 20],
+                        vec![22, 11, 13, 6, 5],
+                        vec![2, 0, 12, 3, 7],
+                    ],
+                },
+            ],
+        };
+
+        let bingo = BingoSubsystem::from(get_input());
+        println!("{:#?}", bingo);
+        assert_eq!(bingo, expected);
+    }
 }
